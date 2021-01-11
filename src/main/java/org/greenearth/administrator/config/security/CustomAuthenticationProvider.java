b7632eab00d6e07d8b1cab982e3de4eb85f617b6
lovenest0821 <lovenest0821@gmail.com>
2021-01-11 21:23:55 +0900

    비밀번호 매치 후 IP, 유효기간 체크

--- src/main/java/org/greenearth/administrator/config/security/CustomAuthenticationProvider.java
+++ src/main/java/org/greenearth/administrator/config/security/CustomAuthenticationProvider.java
@@ -41,21 +41,24 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
         UserAccount userAccount = (UserAccount) userDetails;
         Account account = userAccount.getAccount();
 
-        // 로그인 한 IP 동일 여부 체크
-        if(!account.getUserIp().equals(username[1])) {
-            logger.info("[" + username[0] + "] 로그인이 허용된 IP가 아닙니다. 접속시도IP: " + username[1]);
-            throw new PermitIpException("로그인이 허용된 IP가 아닙니다.");
-        }
-
-        // 비밀번호 변경 기간 만료
-        if(account.getPasswordChangeAt().plusMonths(3).isBefore(LocalDateTime.now())){
-            logger.info("[" + username[0] + "] 비밀번호 유효기간 만료. 이전 변경일자: " + account.getPasswordChangeAt() +
-                    ", 현재일자: "+ LocalDateTime.now() + ", 접속시도IP: " + username[1]);
-            throw new CredentialsExpiredException("비밀번호 유효기간이 만료되었습니다. 비밀번호를 변경하여 주시기 바랍니다.");
-        }
-
         // 비밀번호 일치 여부
-        if(!passwordEncoder.matches(credentials, userDetails.getPassword())) {
+        if(passwordEncoder.matches(credentials, userDetails.getPassword())) {
+            // 로그인 한 IP 동일 여부 체크
+            if(!account.getUserIp().equals(username[1])) {
+                logger.info("[" + username[0] + "] 로그인이 허용된 IP가 아닙니다. 접속시도IP: " + username[1]);
+                throw new PermitIpException("로그인이 허용된 IP가 아닙니다.");
+            }
+
+            // 비밀번호 변경 기간 만료
+            if(account.getPasswordChangeAt().plusMonths(3).isBefore(LocalDateTime.now())){
+                logger.info("[" + username[0] + "] 비밀번호 유효기간 만료. 이전 변경일자: " + account.getPasswordChangeAt() +
+                        ", 현재일자: "+ LocalDateTime.now() + ", 접속시도IP: " + username[1]);
+                throw new CredentialsExpiredException("비밀번호 유효기간이 만료되었습니다. 비밀번호를 변경하여 주시기 바랍니다.");
+            }
+
+            logger.info("["+ username[0] +"] 로그인에 성공하였습니다. " + "접속시도IP: " + username[1]);
+            accountService.initializePasswordErrorCount(account);
+        } else {
             Account updatePasswordErrorCountAndAccountLock = accountService.updatePasswordErrorCountAndAccountLock(account);
             int errorCount = updatePasswordErrorCountAndAccountLock.getPasswordErrorCount();
 
@@ -68,9 +71,6 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
                         + "접속시도IP: " + username[1]);
                 throw new BadCredentialsException("비밀번호 "+ errorCount +"회 오류 입니다.");
             }
-        } else {
-            logger.info("["+ username[0] +"] 로그인에 성공하였습니다. " + "접속시도IP: " + username[1]);
-            accountService.initializePasswordErrorCount(account);
         }
 
         return new UsernamePasswordAuthenticationToken(userDetails, credentials, userDetails.getAuthorities());
