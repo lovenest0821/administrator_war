1cfc79ac8d74e100fa55cda942163b8b3e9ecd7b
lovenest0821 <lovenest0821@gmail.com>
2021-01-12 21:31:51 +0900

    설정 수정

--- src/main/java/org/greenearth/administrator/AdministratorApplication.java
+++ src/main/java/org/greenearth/administrator/AdministratorApplication.java
@@ -2,7 +2,6 @@ package org.greenearth.administrator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
@@ -10,10 +9,5 @@ public class AdministratorApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(AdministratorApplication.class, args);
-    }
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(AdministratorApplication.class);
     }
 }
