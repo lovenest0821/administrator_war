606c55e55aee2e0e4cb22ea0587176d07b15cfa4
lovenest0821 <lovenest0821@gmail.com>
2021-01-16 21:14:53 +0900

    불필요한 import 제거

--- src/main/java/org/greenearth/administrator/config/security/CustomSessionRepositoryFilter.java
+++ src/main/java/org/greenearth/administrator/config/security/CustomSessionRepositoryFilter.java
@@ -1,7 +1,5 @@
 package org.greenearth.administrator.config.security;
 
-import org.greenearth.administrator.account.UserAccount;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.web.filter.GenericFilterBean;
