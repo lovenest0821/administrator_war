139e59abbdf993f380245b5dcd411879f691d50f
lovenest0821 <lovenest0821@gmail.com>
2021-02-12 15:56:24 +0900

    2차 인증관련 Interceptor 추가

--- src/main/java/org/greenearth/administrator/config/CertificationInterceptor.java
+++ src/main/java/org/greenearth/administrator/config/CertificationInterceptor.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 public class CertificationInterceptor implements HandlerInterceptor {
 
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
         /* Todo */
         return true;
     }
